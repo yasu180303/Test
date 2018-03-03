90863016a945d095e23e17c6c41ae7fa77aa86b6
yasu180303 <maelstrom1030@gmail.com>
2018-03-03 16:21:21 +0900

    push to the branch

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
+import sys
+
 def hello(name):
     return "Hello, {0}".format(name)
     
 def bye(name):
     return "Bye, {0}".format(name)
+    
+def main():
+    name = sys.argv[1]
+    print(hello(name))
+    print(bye(name))
+    
         
